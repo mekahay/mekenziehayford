aeb16b659bc334d260101d2a265e65928d9d1df4
mekahay <mekenziehayford@gmail.com>
2021-02-16 11:08:34 -0700

    minor changes with resume link and home page text

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -13,7 +13,7 @@ function Resume() {
         <Card className="resume">
             <Card.Body>
                 <Card.Text>
-                <Button variant="light"><a href='https://docs.google.com/document/d/1LMaMOk228rsMP8rk9ZkKRBkEYgUJQn0vOI5BoLxO7bo/edit?usp=sharing' class='download' download>Download</a></Button>
+                <Button variant="light"><a href='https://docs.google.com/document/d/1LMaMOk228rsMP8rk9ZkKRBkEYgUJQn0vOI5BoLxO7bo/edit?usp=sharing' class='download' target="_blank" download>Download</a></Button>
                 </Card.Text>
             </Card.Body>
             <Card.Img className="justify-content-center" variant="bottom" src={resume} />
